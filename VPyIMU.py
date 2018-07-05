507e08fc407b26c9cb534a0281af297c84275863
mkaca <dabestineuro@gmail.com>
2018-07-04 20:19:31 -0700

    expanded orientation

--- VPyIMU.py
+++ VPyIMU.py
@@ -25,22 +25,22 @@ phalanx_ratios_pinky = [1,1.1,2.1,3.4]
 
 #### based on IMU angle, determine finger position... 
 ###   assume straight knuckle atm
-
+userspin = True
 # length = distal length
 # Only working in 2D ....dw about z 
-def makeFinger (spawnPos,ratio, length, angle):
+def makeFinger (spawnPos,ratio, length, angle, f):
 
 	x = spawnPos[0]
 	y = spawnPos[1]
 	z = spawnPos[2]
 	angle = m.radians(angle)
-	prox = cylinder(pos = (x,y,z), axis = (length*ratio[2],0,0), radius = 1.2)
+	prox = cylinder(frame = f, pos = (x,y,z), axis = (length*ratio[2],0,0), radius = 1.2)
 
 	xStart = x + length*ratio[2]
 	yStart = y
 	axisX = m.cos(angle/2.0)*length*ratio[1]
 	axisY = m.sin(angle/2.0)*length*ratio[1]
-	med = cylinder(pos = (xStart, yStart,z), \
+	med = cylinder(frame = f, pos = (xStart, yStart,z), \
 		axis = (axisX, axisY,0), \
 		radius = 1.1)
 
@@ -48,23 +48,42 @@ def makeFinger (spawnPos,ratio, length, angle):
 	yStart = y + m.sin(angle/2.0)*length*ratio[1]
 	axisX = m.cos(angle)*length*ratio[0]
 	axisY = m.sin(angle)*length*ratio[0]
-	dist = cylinder(pos = (xStart,yStart,z), \
+	dist = cylinder(frame = f, pos = (xStart,yStart,z), \
 		axis = (axisX, axisY, 0),\
 		 radius = 1) 
 	return prox,med,dist
 
-initPos = [2,1,0]
+initPosI = [2,1,0]
+initPosM = [3,1,4]
+initPosR = [2.2,1,8]
 degree = 25
-
+f = frame()
+f.rotate(angle = 1.1)    ## rotates frame for better view
 ### Move finger model 
 for degree in range(0,180,5):
-	prox, med, dist = makeFinger(initPos, phalanx_ratios_index, 5, degree)
-	time.sleep(0.025)
-	dist.visible = False
-	med.visible = False
-	prox.visible = False
-	del dist,med,prox
+	proxI, medI, distI = makeFinger(initPosI, phalanx_ratios_index, 5, degree, f)
+	proxM, medM, distM = makeFinger(initPosM, phalanx_ratios_mid, 6, degree, f)
+	proxR, medR, distR = makeFinger(initPosR, phalanx_ratios_ring, 5, degree, f)
 
+	time.sleep(0.025)
+	distI.visible = False
+	distM.visible = False
+	distR.visible = False
+	medI.visible = False
+	medM.visible = False
+	medR.visible = False
+	proxI.visible = False
+	proxM.visible = False
+	proxR.visible = False
+	del distI, medI,proxI, distM, medM, proxM, distR, proxR, medR
+
+""" TO MAKE THIS WORK WITH FULL FINGER MOTION RANGE: 
+
+- take calculated angle from strain gage = angKnuck
+- ang of mid to prox = IMUAngle - angKnuck
+- ang of prox to horizontal plane = angKnuck
+
+"""
 
 exit()
 
