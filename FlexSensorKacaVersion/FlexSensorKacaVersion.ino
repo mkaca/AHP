df5a48ed6f3658970a304cc9b4b7e019adbd54df
mkaca <dabestineuro@gmail.com>
2018-08-05 16:21:59 -0700

    updated libs

--- FlexSensorKacaVersion/FlexSensorKacaVersion.ino
+++ FlexSensorKacaVersion/FlexSensorKacaVersion.ino
@@ -11,6 +11,7 @@ voltage at A0 should decrease.
 
 ******************************************************************************/
 #include <Sort.h>
+#include <math.h>
 
 const int FLEX_PIN = A0; // Pin connected to voltage divider output
 
@@ -21,7 +22,7 @@ const float STRAIGHT_RESISTANCE = 37300.0; // resistance when straight
 const float BEND_RESISTANCE = 90000.0; // resistance at 90 deg
 
 float calStrRes = 0; //calibrated straight resistance
-
+Sort sortobject = Sort();
 
 
 void setup() 
@@ -29,13 +30,16 @@ void setup()
   Serial.begin(9600);
   pinMode(FLEX_PIN, INPUT);
   //calibrate for straight resistance
-  int arrSize = 5;
-  float[] arr = new float[arrSize];
-  for (float i = 0; i++; i < arrSize){
-    arr[i] = analogRead(FLEX_PIN) * VCC / 1023.0;
+  int arrSize = 7;
+  float arrayCalib[arrSize];
+  for (int i = 0; i++; i < arrSize){
+    arrayCalib[i] = analogRead(FLEX_PIN) * VCC / 1023.0;
   }
-  sort(arr, arr + arrSize);
-  println("Array looks like this: ",arr);
+  float* arrayFlex = sortobject.sortArray(arrayCalib, arrSize);
+  for (int j=0;j++; j < arrSize){
+  Serial.print("Array looks like this:");
+  Serial.println(arrayFlex[j]);
+  }
 }
 
 void loop() 
@@ -44,7 +48,11 @@ void loop()
   int flexADC = analogRead(FLEX_PIN);
   float flexV = flexADC * VCC / 1023.0;
   //float flexR = R_DIV * (VCC / flexV - 1.0);
-  Serial.println("flexV",flexV);
+  Serial.print("flexV");
+  Serial.println(flexADC);
+  Serial.println(flexV);
+  Serial.println(" ");
+  delay(2000);
   //Serial.println("Resistance: " + String(flexR) + " ohms");
 
   // Use the calculated resistance to estimate the sensor's
